07b2e51317bfee4ad2d36b053b5aa1d53ec96541
NEAR <near5@outlook.com>
2017-04-17 12:42:58 -0500

    Imagen fondo y procedimientos en sql
    agregue la imagen de fondo y puse en comentarios unos ejemploos sobre
    'funciones' en sql para insert o usuarios repetidos

--- login.php
+++ login.php
@@ -10,9 +10,42 @@ $query_tipoUsu = "SELECT id_tipo_usuario, nombre_tipo_usuario FROM tipo_usuario 
 $result_tipoUsu = mysqli_query($link, $query_tipoUsu) or die('Error de Conexión (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
 $documentoUsuario = mysqli_query($link, "SELECT * FROM usuarios WHERE documento = '$inputDocumento'");
 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bibliocom Login</title>
+        <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/estilosBiblioteca.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="js/sha.js"></script>
+    <script src="js/jquery-slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+<style type="text/css">
+    .imagenFondo{
+        background: url("images/descarga.jpg");
+        height: 100vh;
+        background-size: cover;
+        background-position: center;
+    }
+    .letra{
+        color: white;
+    }
+</style>
+</head>
+<body class="imagenFondo">
+<div class="container">
+<?php 
 $usuarioExiste = mysqli_query($link, "SELECT * FROM persona WHERE num_documento = '$inputNumeroDoc'");//Hace la consulta para saber si hay un documento con el mismo numero de documento
 if (mysqli_num_rows($usuarioExiste) == 0){ //devuele el numero de documentos encontrados con ese numero y si es 0 significa que no hay ninguno y se realizan los insert
-    $ins = $link -> query("INSERT INTO persona VALUES ('', '$inputTipoDoc', '$inputNumeroDoc', '$inputNombre','$inputApellido', '$inputTelefono', '$inputDireccion', '$inputTipoUsu')");//Estructura para un insert
+    $ins = $link -> query("CALL registro_personas('$inputTipoDoc', '$inputNumeroDoc', '$inputNombre','$inputApellido', '$inputTelefono', '$inputDireccion', '$inputTipoUsu')");
+    //$ins = $link -> query("INSERT INTO persona VALUES ('', '$inputTipoDoc', '$inputNumeroDoc', '$inputNombre','$inputApellido', '$inputTelefono', '$inputDireccion', '$inputTipoUsu')");//Estructura para un insert
     $tablaUsuarios = $link -> query("INSERT INTO usuarios VALUES('','$inputNumeroDoc', '$inputPass')");
     if ($ins) {
     echo '<div class="container" style="margin-top: 100px">
@@ -38,31 +71,12 @@ else{//si el sqli_num_rows devuelve mas de 0 entonces muestra este mensaje de er
     </div>
     ';
 }
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Bibliocom Login</title>
-        <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/estilosBiblioteca.css">
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <!-- jQuery first, then Tether, then Bootstrap JS. -->
-    <script src="js/sha.js"></script>
-    <script src="js/jquery-slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-    <script src="js/bootstrap.min.js"></script>
-</head>
-<body>
-<div class="container">
+ ?>
 <div class="row"> 
 <div class="col"></div>
 <div class="col">
-    <div class="jumbotron" style="margin-top: 10%">
-
-                <h1 class="text-center">Bienvenido</h1>
+    <div class="" style="margin-top: 20%">
+                <h1 class="text-center letra">Bienvenido</h1>
           <div class="row">
             <div class="col"></div>
             <div class="col">
@@ -196,3 +210,67 @@ else{//si el sqli_num_rows devuelve mas de 0 entonces muestra este mensaje de er
 </div>
 </body>
 </html>
+<!--
+delimiter //
+create procedure registro_tipo_persona (in _nombre_tipo_persona varchar(20), in _descripcion text(100))
+begin
+insert into tipo_persona(nombre_tipo_persona, descripcion)values(_nombre_tipo_persona, descripcion);
+end
+
+
+call registro_tipo_persona("Administrador", "descripcion del administrado");
+
+
+funciona como una funcion, la funcion es registro_tipo_persona y recibe dos parametros el nombre del tipo de la persona y la descripcion de la persona 
+
+
+
+
+ejemplo en mi base de datos
+
+DELIMITER //
+CREATE PROCEDURE registro_personas(
+    IN _id_tipo_doc_fk int(10),
+    IN _num_documento bigint(20),
+    IN _nombre varchar(30),
+    IN _apellido varchar(30),
+    IN _telefono int(10),
+    IN _direccion varchar(40),
+    IN _id_tipo_usuario_fk int(10))
+    BEGIN
+    INSERT INTO persona(id_tipo_doc_fk, num_documento, nombre, apellido, telefono, direccion, id_tipo_usuario_fk)
+    VALUES(_id_tipo_doc_fk, _num_documento, _nombre, _apellido, _telefono, _direccion, _id_tipo_usuario_fk);
+    END
+    //
+
+    CALL registro_personas('1', '9540', 'Leonel', 'Messi', '47000', 'barcelona', '2')
+
+Código para verificar si hay un registro guardado con el mismo tipo de documento 
+
+    DROP DATABASE IF EXIST nombre_database;
+    DELIMITER //
+    CREATE PROCEDURE nombre_procedimiento
+    (
+        IN _id_tipo_doc_fk int(10),
+        IN _num_documento bigint(20),
+        IN _nombre varchar(30),
+        IN _apellido varchar(30),
+        IN _telefono int(10),
+        IN _direccion varchar(40),
+        IN _id_tipo_usuario_fk int(10)
+    )
+    BEGIN
+        DECLARE existe_doc int(10);
+        SELECT COUNT(*) INTO existe_doc FROM persona
+        WHERE num_documento = _num_documento;
+        IF existe_doc = 0 THEN
+            INSERT INTO persona(id_tipo_doc_fk, num_documento, nombre, apellido, telefono, direccion, id_tipo_usuario_fk)
+            VALUES(_id_tipo_doc_fk, _num_documento, _nombre, _apellido, _telefono, _direccion, _id_tipo_usuario_fk);
+        ELSE
+            SELECT "El número de documento ya está registrado";
+        END IF;
+        END;
+        //
+
+        CALL num_doc_repetido('1', '1019102219', 'Leonel', 'Suarez', '47000', 'barcelona', '2')
+
